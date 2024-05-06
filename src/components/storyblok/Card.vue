924dc6612c8704be0f53a6013eedc6818f825644
Frederik Bußmann <frederik.bussmann@bestit.de>
2024-05-06 23:48:05 +0200

    Added card component border animation

--- src/components/storyblok/Card.vue
+++ src/components/storyblok/Card.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 
 <template>
     <div
-        :class="['group relative rounded-lg bg-white p-4']"
+        :class="['group relative rounded-lg bg-white px-6 py-6']"
     >
         <h3 class="font-semibold mb-4 leading-1">
             {{ props.blok.title }}
@@ -16,39 +16,10 @@ const props = defineProps<{
 
         <span class="block mb-2">{{ props.blok.description }}</span>
 
-        <Button>
+        <Button class="mb-1">
             Test
         </Button>
 
-        <div
-            :class="[
-                'absolute top-[12px] left-1 h-4 w-0.5 bg-zinc-800',
-                'transition-all ease-out duration-200',
-                'group-hover:top-[calc(100%-28px)]',
-            ]"
-        />
-        <div
-            :class="[
-                'absolute top-[12px] left-1 w-4 h-0.5 bg-zinc-800',
-                'transition-all ease-out duration-200',
-                'group-hover:left-[calc(100%-20px)]',
-            ]"
-        />
-
-        <div
-            :class="[
-                'absolute bottom-[12px] right-1 h-4 w-0.5 bg-zinc-800',
-                'transition-all ease-out duration-200',
-                'group-hover:bottom-[calc(100%-28px)]',
-            ]"
-        />
-
-        <div
-            :class="[
-                'absolute bottom-[12px] right-1 w-4 h-0.5 bg-zinc-800',
-                'transition-all ease-out duration-200',
-                'group-hover:right-[calc(100%-20px)]',
-            ]"
-        />
+        <AnimatedBorder />
     </div>
 </template>
